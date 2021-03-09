c177d81b317b2b53a1c70a729a2af8e72cfda464
Martin Håhjem Årdal <martaard@stud.ntnu.no>
2021-03-09 16:00:39 +0100

    Added new component for a chessboard.

---
+++ sjakkskjerm-frontend/src/components/Live/ChessBoardView.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="chessboardview">
+        <h1>Første brett...</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "ChessBoardView"
+}
+</script>
--- sjakkskjerm-frontend/src/components/Live/ChessBoards.vue
+++ sjakkskjerm-frontend/src/components/Live/ChessBoards.vue
@@ -1,11 +1,17 @@
 <template>
     <div class="chessboards">
         <h1>Her kommer sjakkbrett...</h1>
+        <ChessBoardView/>
     </div>
 </template>
 
 <script>
+import ChessBoardView from "@/components/Live/ChessBoardView.vue";
+
 export default {
-    name: "ChessBoards"
+    name: "ChessBoards",
+    components: {
+        ChessBoardView
+    }
 }
 </script>
