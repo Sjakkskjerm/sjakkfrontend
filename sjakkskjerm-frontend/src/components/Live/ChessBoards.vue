79bbd400bd01cd0b2133a7651e880b0173b7a762
Martin Håhjem Årdal <martaard@stud.ntnu.no>
2021-03-16 14:52:45 +0100

    Fix broken path

--- sjakkskjerm-frontend/src/components/Live/ChessBoards.vue
+++ sjakkskjerm-frontend/src/components/Live/ChessBoards.vue
@@ -1,17 +1,17 @@
 <template>
-    <div class="chessboards">
-        <h1>Her kommer sjakkbrett...</h1>
-        <ChessBoardView/>
-    </div>
+  <div class="chessboards">
+    <h1>Her kommer sjakkbrett...</h1>
+    <ChessBoardView />
+  </div>
 </template>
 
 <script>
-import ChessBoardView from "@/components/live/ChessBoardView.vue";
+import ChessBoardView from "@/components/live/boardview/ChessBoardView.vue";
 
 export default {
-    name: "ChessBoards",
-    components: {
-        ChessBoardView
-    }
-}
+  name: "ChessBoards",
+  components: {
+    ChessBoardView
+  }
+};
 </script>
