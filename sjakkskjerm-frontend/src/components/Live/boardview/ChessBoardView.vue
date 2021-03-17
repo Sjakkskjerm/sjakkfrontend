7c35dd299668d7d9274b737db6a2d7080f701e68
Martin Håhjem Årdal <martaard@stud.ntnu.no>
2021-03-17 13:26:08 +0100

    ChessBoardView now updates board based on API-call.

--- sjakkskjerm-frontend/src/components/Live/boardview/ChessBoardView.vue
+++ sjakkskjerm-frontend/src/components/Live/boardview/ChessBoardView.vue
@@ -8,24 +8,57 @@
 
 <script>
 import ChessBoard from "@/components/live/boardview/ChessBoard.vue";
+import Chess from "chess.js";
+import GameService from "@/services/GameService.js";
 
 export default {
   name: "ChessBoardView",
   components: {
     ChessBoard
   },
+  props: {
+    boardIdentifier: {
+      type: String,
+      required: true
+    }
+  },
   data() {
     return {
-      fen: null
+      boardId: "123",
+      fen: null,
+      game: {},
+      interval: 0,
+      pgn: []
     };
   },
   mounted() {
-    this.$refs.board.position(this.currentfen);
+    this.game = new Chess();
+    this.$refs.board.position("start");
+    this.interval = setInterval(() => {
+      //AXIOS kall. evt, metode som kaller axios kall, som venter på svar, setter pgn og og kaller updateBoardState().
+      this.setPGN();
+    }, 10000);
+  },
+  unmounted() {
+    clearInterval(this.interval);
   },
   methods: {
     updateBoardState() {
-      this.fen = "rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq e3 0 1";
+      // this.fen = "rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq e3 0 1";
+      this.game.load_pgn(this.pgn.join("\n"));
+      this.fen = this.game.fen();
       this.$refs.board.position(this.fen);
+    },
+    setPGN() {
+      GameService.getPgn(this.boardId)
+        .then(response => {
+          this.pgn = response.data.pgn;
+          this.updateBoardState();
+        })
+        .catch(error => {
+          console.log("Unable to set PGN for board: " + this.boardId);
+          console.log(error);
+        });
     }
   }
 };
