9764d7864f9ef2cc2008cf1f58caf7a3a67d1157
Martin Håhjem Årdal <martaard@stud.ntnu.no>
2021-03-26 18:04:19 +0100

    Add error handling, user feedback messages
    Add additional errorhandling, add user feedback measures.
    
    This commit will:
    Add user feedback to BoardManager component
    - for when fetching game ids
    - for certain errors.
    Add errorhandling and debug-messages
    - for Http error response statuses (4xx,5xx)
    - for when request fails.

--- sjakkskjerm-frontend/src/components/Live/boardview/BoardManager.vue
+++ sjakkskjerm-frontend/src/components/Live/boardview/BoardManager.vue
@@ -1,25 +1,29 @@
 <template>
-  <div v-if="gamesFectched" class="boardmanager">
-    <!-- <div v-for="game in games" :key="game">
-    </div> -->
-    <chess-board-view
-      v-for="game in games"
-      :key="game"
-      :game-id="game"
-    ></chess-board-view>
+  <div v-if="error == false" class="boardmanager">
+    <div v-if="gamesFectched" class="boards">
+      <ChessBoardView v-for="game in games" :key="game" :game-id="game" />
+    </div>
+    <div v-else-if="gamesFectched == false">
+      <p>{{ loadText }}</p>
+    </div>
+  </div>
+  <div v-else-if="error">
+    <p>{{ errorDisplayText }}</p>
   </div>
 </template>
 
 <script>
+/*
+    Author: Martin Håhjem Årdal
+    Adds as many ChessBoardView components as there are games
+    being played on digital chess boards in a tournament.
+    Takes in tournamentId as a prop. TournamentId will be used to fetch the
+    number of games of being played on digital chess boards in that tournament.
+*/
 import ChessBoardView from "@/components/live/boardview/ChessBoardView.vue";
 import GameService from "@/services/GameService.js";
 
 export default {
-  //Author: Martin Håhjem Årdal
-  //Adds as many ChessBoardView components as there are games
-  //being played on digital chess boards in a tournament.
-  //Takes in tournamentId as a prop. TournamentId will be used to fetch the
-  //number of games of being played on digital chess boards in that tournament.
   name: "BoardManager",
   components: {
     ChessBoardView
@@ -34,7 +38,10 @@ export default {
   data: function() {
     return {
       games: [],
-      gamesFectched: false
+      gamesFectched: false,
+      loadText: "Fetching games...",
+      error: false,
+      errorDisplayText: ""
     };
   },
   mounted() {
@@ -52,11 +59,27 @@ export default {
           console.log("FetchGames: Succesfully fetched games");
         })
         .catch(error => {
+          this.error = true;
           console.log(
             "FetchGames: Error - Unable to retrieve games for tournament " +
               this.tournamentId
           );
-          console.log(error);
+          if (error.response) {
+            if (error.response.status == 400) {
+              this.errorDisplayText = "Bad Request";
+            } else if (error.response.status == 403) {
+              this.errorDisplayText = "Forbidden";
+            } else if (error.response.status == 404) {
+              this.errorDisplayText =
+                "Tournament " + this.tournamentId + " does not exist.";
+            } else if (error.response.status == 500) {
+              this.errorDisplayText = "Server error.";
+            }
+          } else if (error.request) {
+            this.errorDisplayText = "Network Error";
+          } else {
+            console.log(error);
+          }
         });
     }
   }
