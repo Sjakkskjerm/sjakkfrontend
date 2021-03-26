8d8792bd84455520ab644d24fbb91b1bb4779cfb
Martin Håhjem Årdal <martaard@stud.ntnu.no>
2021-03-26 12:21:16 +0100

    Add Board Manager, now display as many boards as needed.
    This commit will add
    BoardManager component. It has a tournament id as a prop. It will ask an API for all game ids for that tournament. When response is given, it will populate its template element with as many ChessBoardView components as there are gameids.

--- sjakkskjerm-frontend/src/components/Live/ChessBoards.vue
+++ sjakkskjerm-frontend/src/components/Live/ChessBoards.vue
@@ -1,17 +1,20 @@
 <template>
   <div class="chessboards">
     <!-- <h1>Her kommer sjakkbrett...</h1> -->
-    <ChessBoardView />
+    <!-- <ChessBoardView /> -->
+    <BoardManager />
   </div>
 </template>
 
 <script>
-import ChessBoardView from "@/components/live/boardview/ChessBoardView.vue";
+// import ChessBoardView from "@/components/live/boardview/ChessBoardView.vue";
+import BoardManager from "@/components/live/boardview/BoardManager.vue";
 
 export default {
   name: "ChessBoards",
   components: {
-    ChessBoardView
+    BoardManager
+    // ChessBoardView
   }
 };
 </script>
