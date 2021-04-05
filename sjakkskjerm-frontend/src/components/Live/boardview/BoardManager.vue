f28b454e3a0ba686ad4ae960e24c90084395b26b
Martin Håhjem Årdal <martaard@stud.ntnu.no>
2021-04-05 20:24:28 +0200

    Add user feedback for possible fetch and error states. Styling component
    This commit will:
    - Add user feedback for other states than showing boards.
    - Add styling to components for more comfortable viewing of boards.

--- sjakkskjerm-frontend/src/components/Live/boardview/BoardManager.vue
+++ sjakkskjerm-frontend/src/components/Live/boardview/BoardManager.vue
@@ -1,14 +1,24 @@
 <template>
   <div v-if="error == false" class="boardmanager">
     <div v-if="gamesFectched" class="boards">
-      <ChessBoardView v-for="game in games" :key="game" :game-id="game" />
+      <div v-if="hasGames">
+        <ChessBoardView
+          v-for="game in games"
+          :key="game"
+          class="board"
+          :game-id="game"
+        />
+      </div>
+      <div v-else>
+        <p>{{ noGamesText }}</p>
+      </div>
     </div>
-    <div v-else-if="gamesFectched == false">
-      <p>{{ loadText }}</p>
+    <div v-else-if="gamesFectched == false" class="load-board-text">
+      <p>{{ fetchText }}</p>
     </div>
   </div>
-  <div v-else-if="error">
-    <p>{{ errorDisplayText }}</p>
+  <div v-else-if="error" class="error-text-boardmanager">
+    <p>{{ errorUserFeedbackText }}</p>
   </div>
 </template>
 
@@ -32,17 +42,23 @@ export default {
     tournamentId: {
       type: String,
       required: true,
-      default: "123" //default value for development.
+      default: "124" //default value for development.
     }
   },
   data: function() {
     return {
       games: [],
       gamesFectched: false,
-      loadText: "Fetching games...",
+      fetchText: "Fetching games..",
+      noGamesText: "Tournament " + this.tournamentId + " has no games.",
       error: false,
-      errorDisplayText: ""
+      errorUserFeedbackText: "Error Occured: "
     };
+  },
+  computed: {
+    hasGames() {
+      return this.games.length > 0;
+    }
   },
   mounted() {
     this.fetchGames();
@@ -56,32 +72,56 @@ export default {
         .then(response => {
           this.games = response.data.games;
           this.gamesFectched = true;
+          this.userFeedbackText = "Tournament has no games";
           console.log("FetchGames: Succesfully fetched games");
         })
         .catch(error => {
           this.error = true;
-          console.log(
-            "FetchGames: Error - Unable to retrieve games for tournament " +
-              this.tournamentId
-          );
-          if (error.response) {
-            if (error.response.status == 400) {
-              this.errorDisplayText = "Bad Request";
-            } else if (error.response.status == 403) {
-              this.errorDisplayText = "Forbidden";
-            } else if (error.response.status == 404) {
-              this.errorDisplayText =
-                "Tournament " + this.tournamentId + " does not exist.";
-            } else if (error.response.status == 500) {
-              this.errorDisplayText = "Server error.";
-            }
-          } else if (error.request) {
-            this.errorDisplayText = "Network Error";
-          } else {
-            console.log(error);
-          }
+          this.errorHandler(error);
         });
+    },
+    errorHandler(error) {
+      console.log(
+        "FetchGames: Error - Unable to retrieve games for tournament " +
+          this.tournamentId
+      );
+      if (error.response) {
+        if (error.response.status == 404) {
+          this.errorUserFeedbackText +=
+            "Tournament " + this.tournamentId + " does not exist.";
+        } else if (error.response.status == 500) {
+          this.errorUserFeedbackText +=
+            "Sjakkskjerm server is down. We are unable to give a recovery time estimate.";
+        } else if (error.response.status == 503) {
+          //Axios Error does not support accessing Retry-After header field. Can't easily give estimate.
+          this.errorUserFeedbackText +=
+            "Sjakkskjerm server is unresponsive. Try again in a few minutes.";
+        } else {
+          this.errorUserFeedbackText +=
+            "Unrecoverable error. We done did something stupid, sry. Please try at another time.";
+          console.log(error.response.data);
+          console.log(error.response.status);
+          console.log(error.response.headers);
+        }
+      } else if (error.request) {
+        //There may be other errors. Hard to assess.
+        this.errorUserFeedbackText +=
+          "Unable to connect to server. There may be an issue with your internet connection.";
+        console.log(error.request);
+      } else {
+        console.log("Error", error.message);
+      }
+      console.log(error.config);
     }
   }
 };
 </script>
+
+<style scoped>
+.boards {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: space-evenly;
+}
+</style>
