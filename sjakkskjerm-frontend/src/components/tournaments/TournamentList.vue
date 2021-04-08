9febd90f75f85afbcbe99ece96d8cc93c355e873
Lasse KH <lasse.2011@hotmail.com>
2021-04-08 10:23:04 +0200

    TournamentList now sorts tournaments based on start date & end date
    Sorts into ongoing tournaments and future tournaments (and ended tournaments)

--- sjakkskjerm-frontend/src/components/tournaments/TournamentList.vue
+++ sjakkskjerm-frontend/src/components/tournaments/TournamentList.vue
@@ -1,8 +1,19 @@
 <template>
   <div v-if="error == false" class="tournamentlist">
-    <h2>Her kommer en liste av turneringer...</h2>
+    <!-- <h2>Her kommer en liste av turneringer...</h2> -->
+    <h1>
+      Ongoing tournaments
+    </h1>
     <TournamentItem
-    v-for="tournament in tournaments"
+    v-for="tournament in ongoingTournaments"
+    :key="tournament"
+    :tournament="tournament"
+    />
+    <h1>
+      Future tournaments
+    </h1>
+     <TournamentItem
+    v-for="tournament in futureTournaments"
     :key="tournament"
     :tournament="tournament"
     />
@@ -24,7 +35,10 @@ export default {
   data() {
     return {
       error: false,
-      tournaments: []
+      tournaments: [],
+      ongoingTournaments: [],
+      futureTournaments: [],
+      endedTournaments: [],
     };
   },
   mounted() {
@@ -35,13 +49,33 @@ export default {
       GameService.getTournaments()
         .then(response => {
           this.tournaments = response.data;
-          console.log(this.tournaments);
+          this.divideTournamentsByDate();
         })
         .catch(error => {
           console.log(error);
           this.error = true;
         });
-    }
+    },
+    divideTournamentsByDate() {
+      const today = new Date();
+      for(let i=0; i < this.tournaments.length; i++) {
+        let tournament = this.tournaments[i];
+        let startDate = new Date(JSON.stringify(tournament.startDate));
+        let endDate = new Date(JSON.stringify(tournament.endDate));
+        if(startDate.getTime() > today.getTime()) {
+          this.futureTournaments.push(tournament);
+        }
+        else if(startDate.getTime() <= today.getTime() && today.getTime() <= endDate.getTime()) {
+          this.ongoingTournaments.push(tournament);
+        }
+        else {
+          this.endedTournaments.push(tournament);                
+        }
+      }
+      console.log(this.futureTournaments);
+      console.log(this.ongoingTournaments);
+      
+    },
   }
 };
 </script>
