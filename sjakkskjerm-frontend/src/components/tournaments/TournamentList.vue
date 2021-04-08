6b427d2fd0d45360f093a03e4634d7b94441faa5
Lasse KH <lasse.2011@hotmail.com>
2021-04-08 11:23:29 +0200

    Cleaned tournamentlist class and moved away sorting functionality

--- sjakkskjerm-frontend/src/components/tournaments/TournamentList.vue
+++ sjakkskjerm-frontend/src/components/tournaments/TournamentList.vue
@@ -1,30 +1,17 @@
 <template>
-  <div v-if="error == false" class="tournamentlist">
-    <!-- <h2>Her kommer en liste av turneringer...</h2> -->
+  <div class="tournamentlist">
     <h1>
-      Ongoing tournaments
+      {{ title }}
     </h1>
     <TournamentItem
-    v-for="tournament in ongoingTournaments"
+    v-for="tournament in tournaments"
     :key="tournament"
     :tournament="tournament"
     />
-    <h1>
-      Future tournaments
-    </h1>
-     <TournamentItem
-    v-for="tournament in futureTournaments"
-    :key="tournament"
-    :tournament="tournament"
-    />
-  </div>
-  <div v-if="error" class="yeye">
-    <p>"hello"</p>
   </div>
 </template>
 
 <script>
-import GameService from "@/services/GameService.js";
 import TournamentItem from "@/components/tournaments/TournamentItem.vue";
 
 export default {
@@ -32,49 +19,15 @@ export default {
   components: {
     TournamentItem,
   },
-  data() {
-    return {
-      error: false,
-      tournaments: [],
-      ongoingTournaments: [],
-      futureTournaments: [],
-      endedTournaments: [],
-    };
-  },
-  mounted() {
-    this.fetchTournaments();
-  },
-  methods: {
-    fetchTournaments() {
-      GameService.getTournaments()
-        .then(response => {
-          this.tournaments = response.data;
-          this.divideTournamentsByDate();
-        })
-        .catch(error => {
-          console.log(error);
-          this.error = true;
-        });
+  props: {
+    tournaments: { 
+      type: Array,
+      required: true,
     },
-    divideTournamentsByDate() {
-      const today = new Date();
-      for(let i=0; i < this.tournaments.length; i++) {
-        let tournament = this.tournaments[i];
-        let startDate = new Date(JSON.stringify(tournament.startDate));
-        let endDate = new Date(JSON.stringify(tournament.endDate));
-        if(startDate.getTime() > today.getTime()) {
-          this.futureTournaments.push(tournament);
-        }
-        else if(startDate.getTime() <= today.getTime() && today.getTime() <= endDate.getTime()) {
-          this.ongoingTournaments.push(tournament);
-        }
-        else {
-          this.endedTournaments.push(tournament);                
-        }
-      }
-      console.log(this.futureTournaments);
-      console.log(this.ongoingTournaments);
-      
+    title: {
+      type: String,
+      required: true,
+      default: "test",
     },
   }
 };
