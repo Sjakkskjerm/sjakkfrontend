bc47f3be8bf2624f25c3ac8b735780fdf3c2fdf7
Lasse KH <lasse.2011@hotmail.com>
2021-04-12 15:34:46 +0200

    Added base input component for scalability and to keep forms consistent
    Can be expanded on to allow more types later

---
+++ sjakkskjerm-frontend/src/components/forms/BaseForm.vue
@@ -0,0 +1,33 @@
+<template>
+    <label>{{ label }}</label>
+    <input
+    v-bind="$attrs"
+    :placeholder="label"
+    class="field"
+    :value="modelValue"
+    @input="$emit('update:modelValue', $event.target.value)"
+    >
+</template>
+
+<script>
+/*
+    Author: Lasse Kregnes Hansen
+    A reusable form component for each specific input type, for easier 
+    replication, modification and extention of forms. Designed to keep 
+    all forms consistent in the system. (Works as an abstraction layer 
+    for a form. See CreateTournament component for an example of use.)
+*/
+
+export default {
+    props: {
+        label: {
+            type: String,
+            default: ''
+        },
+        modelValue: {
+            type: [String, Number],
+            default: ''
+        }
+    }
+}
+</script>
