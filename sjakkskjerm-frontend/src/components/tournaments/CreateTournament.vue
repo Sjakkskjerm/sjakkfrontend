c4e2a67a8f33a220877508e49012ecfccbbc9106
Lasse KH <lasse.2011@hotmail.com>
2021-04-13 15:03:22 +0200

    Tournaments can now be created and saved in the DB

--- sjakkskjerm-frontend/src/components/tournaments/CreateTournament.vue
+++ sjakkskjerm-frontend/src/components/tournaments/CreateTournament.vue
@@ -1,42 +1,40 @@
 <template>
   <div>
     <h1>Create a tournament</h1>
-    <form>
+    <form @submit.prevent="sendForm">
       <h3>Name your tournament</h3>
 
-      <BaseInput v-model="tournamentName" label="Name" type="text" />
+      <BaseInput v-model="tournament.tournamentName" label="Name" type="text" />
 
       <h3>Set the duration of the tournament</h3>
 
       <label>Start date</label>
       <datepicker
-        v-model="startDate"
-        :locale="locale"
-        :upper-limit="to"
-        :lower-limit="from"
+        v-model="tournament.startDate"
+        placeholder="Start date (Click to choose)"
       />
 
       <label>End date</label>
       <datepicker
-        v-model="endDate"
-        :locale="locale"
-        :upper-limit="to"
-        :lower-limit="from"
+        v-model="tournament.endDate"
+        placeholder="End date (Click to choose)"
       />
 
       <h3>How many rounds, and games per round will be played?</h3>
 
       <BaseInput
-        v-model.number="numberOfRounds"
+        v-model.number="tournament.numberOfRounds"
         label="Number of rounds"
         type="number"
       />
 
       <BaseInput
-        v-model.number="numberOfGames"
+        v-model.number="tournament.numberOfGames"
         label="Number of games per round"
         type="number"
       />
+
+      <button type="submit">Create</button>
     </form>
   </div>
 </template>
@@ -44,6 +42,7 @@
 <script>
 import BaseInput from "../forms/BaseInput.vue";
 import Datepicker from "vue3-datepicker";
+import axios from "axios";
 
 export default {
   name: "CreateTournament",
@@ -53,12 +52,30 @@ export default {
   },
   data() {
     return {
-      tournamentName: "",
-      startDate: "",
-      endDate: "",
-      numberOfRounds: "",
-      numberOfGames: ""
+      tournament: {
+        tournamentName: "",
+        startDate: new Date(),
+        endDate: null,
+        numberOfRounds: "",
+        gamesPerRound: ""
+      }
     };
+  },
+  methods: {
+    sendForm() {
+      console.log("yoyoyoy" + this.tournament)
+      axios
+        .post(
+          "http://localhost:8080/api/tournaments/createtournament",
+          this.tournament
+        )
+        .then(function(response) {
+          console.log("response", response);
+        })
+        .catch(function(err) {
+          console.log("Error", err);
+        });
+    }
   }
 };
 </script>
