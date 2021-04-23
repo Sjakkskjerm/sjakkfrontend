d3fc78730846ff67513626dff5c8a52c9ade54ee
Pål Nyseth <paalnyseth@gmail.com>
2021-04-23 15:23:35 +0200

    Fixed message

--- sjakkskjerm-frontend/src/components/live/messages/MessageView.vue
+++ sjakkskjerm-frontend/src/components/live/messages/MessageView.vue
@@ -1,14 +1,6 @@
 <template>
-    <div class = "melding_wrap">
-        <div class="tittel">
-            {{  "Melding fra arrangør "}}
-        </div>
-        <div class="dato">
-            {{" - " + message.dato}}
-        </div>
-        <div class="melding">
-            {{" - " + message.melding }}
-        </div>
+    <div class = "message-card">
+        <p class="message">{{message.date}}: {{message.message}}</p>
     </div>
 </template>
 
@@ -26,4 +18,21 @@ export default {
 </script>
 
 <style scoped>
+.message-card {
+  padding: 10px;
+  cursor: pointer;
+  margin-bottom: 1em;
+  margin-right: 35%;
+  margin-left: 35%;
+}
+.message-card:hover {
+  transform: scale(1.01);
+  box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
+}
+
+.message {
+  font-style: italic;
+  margin: 0%;
+  padding: 0%;
+}
 </style>
