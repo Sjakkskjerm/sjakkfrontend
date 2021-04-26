1104254f8045d2e75c02525e84af87a2f524f57a
Martin Håhjem Årdal <martaard@stud.ntnu.no>
2021-04-26 14:32:28 +0200

    Clean up GameService

--- sjakkskjerm-frontend/src/services/GameService.js
+++ sjakkskjerm-frontend/src/services/GameService.js
@@ -1,28 +1,22 @@
 import axios from "axios";
 
+//prod
+// const prodBackend = "http://158.38.101.216";
+
 //local
-const localBackendUrl = "http://localhost:8080";
-const localBackendTournaments = "/api/tournaments/";
-const localBackendGames = "/api/games/";
-const localBackendGamesByTournament = "/api/games/gamesfortournament/";
+const localBackend = "http://localhost:8080/api";
+
+//paths
+const tournaments = "/tournaments/";
+const games = "/games/";
+const gamesByTournament = "/games/gamesfortournament/";
+const message = "/message";
+const specificMessage = "/message/specific";
+const login = "auth/login";
+const register = "auth/register";
 
 const apiClient = axios.create({
-  baseURL: localBackendUrl,
-  withCredentials: false,
-  headers: {
-    Accept: "application/json",
-    "Content-Type": "application/json"
-  }
-});
-
-const baseURL_API = "http://localhost:8080/api";
-const messagesURL = "/message";
-const messageSpecificURL = "/message/specific";
-const loginURL = "auth/login";
-const registerURL = "auth/register";
-
-const apiClient_1 = axios.create({
-  baseURL: baseURL_API,
+  baseURL: localBackend,
   withCredentials: false,
   headers: {
     Accept: "application/json",
@@ -32,31 +26,29 @@ const apiClient_1 = axios.create({
 
 export default {
   getPgn(gameId) {
-    return apiClient.get(localBackendGames, { params: { id: gameId } });
+    return apiClient.get(games, { params: { id: gameId } });
   },
   getGames(tournamentId) {
-    return apiClient.get(localBackendGamesByTournament, {
+    return apiClient.get(gamesByTournament, {
       params: { tournamentid: tournamentId }
     });
   },
   getTournaments() {
-    return apiClient.get(localBackendTournaments);
+    return apiClient.get(tournaments);
   },
   getMessages() {
-    return apiClient_1.get(messagesURL);
+    return apiClient.get(message);
   },
   getMessage(tournamentId) {
-    return apiClient_1.get(
-      messageSpecificURL + "?tournamentId=" + tournamentId
-    );
+    return apiClient.get(specificMessage + "?tournamentId=" + tournamentId);
   },
   sendMessages(data) {
-    return apiClient_1.post(messagesURL, data);
+    return apiClient.post(message, data);
   },
   login(data) {
-    return apiClient_1.post(loginURL, data);
+    return apiClient.post(login, data);
   },
   register(data) {
-    return apiClient_1.post(registerURL, data);
+    return apiClient.post(register, data);
   }
 };
