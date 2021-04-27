78c41ac7c22ebaf230e623fdee25ab645bde8137
Martin Håhjem Årdal <martaard@stud.ntnu.no>
2021-04-27 11:12:29 +0200

    Remove button that routes to send message.

--- sjakkskjerm-frontend/src/components/userconfig/Dashboard.vue
+++ sjakkskjerm-frontend/src/components/userconfig/Dashboard.vue
@@ -6,9 +6,6 @@
     <div>Bruker ID: {{gettersAuthData.uid}}</div>
     <div>Roller: {{gettersAuthData.role}}</div>
     <br>
-    <button @click="$router.push('/admin/sendmessage')" 
-    v-if="gettersAuthData.role === 'ROLE_ORGANIZER'"
-    class="btn btn-primary">Send Melding</button>
   </div>
 </template>
 
