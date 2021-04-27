2892f3bf3d45c1a02f51d6cf84a86432e9534359
Martin Håhjem Årdal <martaard@stud.ntnu.no>
2021-04-27 09:59:01 +0200

    Add backend URL.

--- sjakkskjerm-frontend/src/services/AuthoService.js
+++ sjakkskjerm-frontend/src/services/AuthoService.js
@@ -1,36 +1,41 @@
 import axios from "axios";
 
+//prod URL
+// const baseURL_API = "http://158.38.101.216:8080/api";
+
+//local URL
 const baseURL_API = "http://localhost:8080/api";
 //const messagesURL = "/message";
 
 const instance = axios.create({
-    baseURL: baseURL_API
+	baseURL: baseURL_API,
 });
 
 instance.interceptors.request.use(
-    config => {
-        const token = localStorage.getItem("access_token");
-        if (token) {
-            config.headers.Authorization = `Bearer ${token}`
-        }
-        return config;
-    },
-    error => Promise.reject(error)
+	(config) => {
+		const token = localStorage.getItem("access_token");
+		if (token) {
+			config.headers.Authorization = `Bearer ${token}`;
+		}
+		return config;
+	},
+	(error) => Promise.reject(error)
 );
 
 const AuthoService = {
-    get(url) {
-        return instance.get(url) 
-            .then(response => response)
-            .catch(reason => Promise.reject(reason));
-    },
+	get(url) {
+		return instance
+			.get(url)
+			.then((response) => response)
+			.catch((reason) => Promise.reject(reason));
+	},
 
-    post(url, data) {
-        return instance.post(url, data)
-            .then(response => response)
-            .catch(reason => Promise.reject(reason));
-    }
-
+	post(url, data) {
+		return instance
+			.post(url, data)
+			.then((response) => response)
+			.catch((reason) => Promise.reject(reason));
+	},
 };
 
 export default AuthoService;
