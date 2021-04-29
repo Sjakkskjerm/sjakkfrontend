9504f2455c3341dc18657d209bd8429a6869b61e
Martin Håhjem Årdal <martaard@stud.ntnu.no>
2021-04-29 15:05:50 +0200

    Add ability to delete tournament. Button only appears if you're an owner

--- sjakkskjerm-frontend/src/views/TournamentDashboard.vue
+++ sjakkskjerm-frontend/src/views/TournamentDashboard.vue
@@ -4,11 +4,20 @@
     <h2>{{ tournament.tournamentName }}</h2>
     <h3>Sendte meldinger</h3>
     <DashboardMessage :tournamentid="id" />
+    <div v-if="ownerOfTournament" class="delete-tournament-div">
+      <button
+        class="btn btn-dark delete-tournament-button"
+        @click="deleteTournament"
+      >
+        Slett turnering
+      </button>
+    </div>
   </div>
 </template>
 
 <script>
 import GameService from "@/services/GameService.js";
+import AuthoService from "@/services/AuthoService.js";
 import DashboardMessage from "@/components/dashboard/DashboardMessage.vue";
 
 export default {
@@ -24,8 +33,21 @@ export default {
   },
   data() {
     return {
-      tournament: ""
+      tournament: "",
+      error: null,
+      errorFeedBackText: ""
     };
+  },
+  computed: {
+    ownerOfTournament() {
+      if (this.tournament != "") {
+        return (
+          this.$store.state.auth.authData.uid == this.tournament.owner.userId
+        );
+      } else {
+        return false;
+      }
+    }
   },
   mounted() {
     this.fetchTournament();
@@ -38,6 +60,19 @@ export default {
         })
         .catch(error => {
           console.log(error);
+        });
+    },
+    deleteTournament() {
+      const deleteTournamentURL =
+        "/tournaments/delete/?tournamentid=" + this.id;
+      AuthoService.delete(deleteTournamentURL)
+        .then(() => {
+          this.$router.push("/");
+        })
+        .catch(error => {
+          this.error = error;
+          console.log(this.error);
+          this.errorFeedBackText = "Kunne ikke slette turnering.";
         });
     }
   }
