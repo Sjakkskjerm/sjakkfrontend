178bbabb6b025c5e933c37b2c2af66c9544e2bd8
Martin Håhjem Årdal <martaard@stud.ntnu.no>
2021-05-07 13:49:49 +0200

    Add a CSS GRID to the outer tournaments.

--- sjakkskjerm-frontend/src/views/Tournament.vue
+++ sjakkskjerm-frontend/src/views/Tournament.vue
@@ -1,7 +1,10 @@
 <template>
-  <div v-if="tournamentId" class="tournament">
-    <ChessBoards :tournament-id="tournamentId" />
-    <MessageBoard :tournament-id="tournamentId" />
+  <div v-if="tournamentId" class="tournament-container">
+    <ChessBoards class="chess-board-container" :tournament-id="tournamentId" />
+    <MessageBoard
+      class="message-board-container"
+      :tournament-id="tournamentId"
+    />
   </div>
 </template>
 
@@ -50,3 +53,23 @@ export default {
   }
 };
 </script>
+
+<style scoped>
+.tournament-container {
+  display: grid;
+  grid-template-columns: 1.8fr 0.2fr;
+  grid-template-rows: 1fr;
+  gap: 0px 0px;
+  grid-template-areas: ". .";
+  height: 96vh;
+}
+.chess-board-container {
+  overflow-y: auto;
+}
+
+.message-board-container {
+  background-color: #e4e4e4;
+  float: right;
+  overflow-y: auto;
+}
+</style>
