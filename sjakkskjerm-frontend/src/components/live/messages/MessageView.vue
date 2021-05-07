fe02ac86b2695bb719ff652686623881efff0894
Martin Håhjem Årdal <martaard@stud.ntnu.no>
2021-05-07 13:51:30 +0200

    Delete button can now be hidden by passing in a prop

--- sjakkskjerm-frontend/src/components/live/messages/MessageView.vue
+++ sjakkskjerm-frontend/src/components/live/messages/MessageView.vue
@@ -1,16 +1,18 @@
 <template>
   <div class="message-card">
-    <p class="message">{{ message.date }}: {{ message.message }}</p>
-    <button
-      v-if="
-        gettersAuthData.role === 'ROLE_ORGANIZER' ||
-          gettersAuthData.role === 'ROLE_ADMIN'
-      "
-      class="btn btn-dark delete-message-button"
-      @click="deleteMsg"
-    >
-      Slett
-    </button>
+    <p class="message">{{ message.message }}</p>
+    <div v-if="deleteButton">
+      <button
+        v-if="
+          gettersAuthData.role === 'ROLE_ORGANIZER' ||
+            gettersAuthData.role === 'ROLE_ADMIN'
+        "
+        class="btn btn-dark delete-message-button"
+        @click="deleteMsg"
+      >
+        Slett
+      </button>
+    </div>
   </div>
 </template>
 
@@ -24,6 +26,11 @@ export default {
     message: {
       type: Object,
       required: true
+    },
+    deleteButton: {
+      type: Boolean,
+      required: false,
+      default: true
     }
   },
   emits: {
@@ -54,14 +61,17 @@ export default {
 
 <style scoped>
 .message-card {
-  padding: 10px;
-  margin-bottom: 1em;
-  margin-right: 5%;
-  margin-left: 5%;
+  padding: 0.5rem;
+  margin: 0 0.2em 0.5em 0.2em;
+  text-align: left;
+  overflow: auto;
+  border-style: solid;
+  border-width: 0.15rem;
+  border-radius: 0.26rem;
+  box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
 }
 
 .message {
-  /* font-style: italic; */
   margin: 0%;
   padding: 0%;
   float: left;
